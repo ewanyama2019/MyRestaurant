44b474ccff0643079f793ad70ec99382ded15ee1
ewanyama2019 <wafulawanyama@gmail.com>
2019-06-20 10:59:13 +0300

    will add ButterKnife and Refactor code in MainActivity.java

--- app/src/main/java/com/wanyama/myrestaurant/MainActivity.java
+++ app/src/main/java/com/wanyama/myrestaurant/MainActivity.java
@@ -12,22 +12,28 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.graphics.Typeface;
+import butterknife.ButterKnife;
+import butterknife.BindView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button mFindRestaurantButton;
-    private EditText mLocationEditText;
-    private TextView mAppNameTextView;
+    @BindView(R.id.findRestaurantButton) Button mFindRestaurantButton;
+    @BindView(R.id.locationEditText) EditText mLocationEditText;
+    @BindView(R.id.appNameTextView) TextView mAppNameTextView;
+//    private Button mFindRestaurantButton;
+//    private EditText mLocationEditText;
+//    private TextView mAppNameTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mAppNameTextView= (TextView) findViewById(R.id.appNameTextView);
+        ButterKnife.bind(this);
+//        mAppNameTextView= (TextView) findViewById(R.id.appNameTextView);
         Typeface ostrichFont = Typeface.createFromAsset(getAssets(), "fonts/ostrich-regular.ttf");
         mAppNameTextView.setTypeface(ostrichFont);
-        mLocationEditText = (EditText) findViewById(R.id.locationEditText);
-        mFindRestaurantButton = (Button) findViewById(R.id.findRestaurantButton);
+//        mLocationEditText = (EditText) findViewById(R.id.locationEditText);
+//        mFindRestaurantButton = (Button) findViewById(R.id.findRestaurantButton);
         mFindRestaurantButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
