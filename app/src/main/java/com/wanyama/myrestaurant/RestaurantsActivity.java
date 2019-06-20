8bf2b9d6ac8fe080d8b781472e95a12649a6bde0
ewanyama2019 <wafulawanyama@gmail.com>
2019-06-20 13:02:58 +0300

    will add ButterKnife and Refactor code in RestaurantesActivity.java

--- app/src/main/java/com/wanyama/myrestaurant/RestaurantsActivity.java
+++ app/src/main/java/com/wanyama/myrestaurant/RestaurantsActivity.java
@@ -11,10 +11,15 @@ import android.widget.TextView;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class RestaurantsActivity extends AppCompatActivity {
 
-    private TextView mLocationTextView;
-    private ListView mListView;
+    @BindView(R.id.locationTextView) TextView mLocationTextView;
+    @BindView(R.id.listView) ListView mListView;
+//    private TextView mLocationTextView;
+//    private ListView mListView;
 
     private String[] restaurants = new String[] {"Mangrove", "Big Square - Wilson",
             "Vibro", "Nixen Entertainment", "Thorntree", "Chicken World Kenya",
@@ -22,13 +27,20 @@ public class RestaurantsActivity extends AppCompatActivity {
             "Maritas Bhajias", "Sikia Fine Dining", "Greenrose",
             "KPM Nyama Choma", "Fish Grill"};
 
+    private String[] cuisines = new String[] {"Vegan Food", "Breakfast", "Fishs Dishs",
+            "Scandinavian", "Coffee", "English Food", "Burgers", "Fast Food", "Noodle Soups",
+            "Mexican", "BBQ", "Cuban", "Bar Food", "Sports Bar", "Breakfast", "Mexican"};
+
+    MyRestaurantsArrayAdapter adapter = new MyRestaurantsArrayAdapter(this, android.R.layout.simple_list_item_1, restaurants, cuisines); // must match constructor!
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurants);
+        ButterKnife.bind(this);
 
-        mListView =(ListView) findViewById(R.id.listView);
-        mLocationTextView = (TextView) findViewById(R.id.locationTextView);
+//        mListView =(ListView) findViewById(R.id.listView);
+//        mLocationTextView = (TextView) findViewById(R.id.locationTextView);
 
         ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, restaurants);
         mListView.setAdapter(adapter);
