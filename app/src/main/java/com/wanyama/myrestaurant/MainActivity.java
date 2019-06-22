bf4b4b806ec56c36c1ed82e39a755ab858ddc905
ewanyama2019 <wafulawanyama@gmail.com>
2019-06-22 18:39:32 +0300

    will add refactor code backwrds by removing butterknife and replaced MainActivity background image with alionabackgrnd_1k.jpg

--- app/src/main/java/com/wanyama/myrestaurant/MainActivity.java
+++ app/src/main/java/com/wanyama/myrestaurant/MainActivity.java
@@ -12,29 +12,26 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.graphics.Typeface;
-import butterknife.ButterKnife;
-import butterknife.BindView;
+
 
 public class MainActivity extends AppCompatActivity {
-
-    @BindView(R.id.findRestaurantButton) Button mFindRestaurantButton;
-    @BindView(R.id.locationEditText) EditText mLocationEditText;
-    @BindView(R.id.appNameTextView) TextView mAppNameTextView;
-//    private Button mFindRestaurantButton;
-//    private EditText mLocationEditText;
-//    private TextView mAppNameTextView;
+    private Button mFindRestaurantsButton;
+    private EditText mLocationEditText;
+    private TextView mAppNameTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ButterKnife.bind(this);
-//        mAppNameTextView= (TextView) findViewById(R.id.appNameTextView);
+
+        mLocationEditText = (EditText) findViewById(R.id.locationEditText);
+        mFindRestaurantsButton = (Button) findViewById(R.id.findRestaurantButton);
+        mAppNameTextView = (TextView) findViewById(R.id.appNameTextView);
+
         Typeface ostrichFont = Typeface.createFromAsset(getAssets(), "fonts/ostrich-regular.ttf");
         mAppNameTextView.setTypeface(ostrichFont);
-//        mLocationEditText = (EditText) findViewById(R.id.locationEditText);
-//        mFindRestaurantButton = (Button) findViewById(R.id.findRestaurantButton);
-        mFindRestaurantButton.setOnClickListener(new View.OnClickListener() {
+
+        mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String location = mLocationEditText.getText().toString();
@@ -42,6 +39,6 @@ public class MainActivity extends AppCompatActivity {
                 intent.putExtra("location", location);
                 startActivity(intent);
             }
-            });
+        });
     }
 }
--- app/src/main/java/com/wanyama/myrestaurant/RestaurantsActivity.java
+++ app/src/main/java/com/wanyama/myrestaurant/RestaurantsActivity.java
@@ -11,36 +11,24 @@ import android.widget.TextView;
 import android.widget.ListView;
 import android.widget.Toast;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
+//import butterknife.BindView;
+//import butterknife.ButterKnife;
 
 public class RestaurantsActivity extends AppCompatActivity {
 
-    @BindView(R.id.locationTextView) TextView mLocationTextView;
-    @BindView(R.id.listView) ListView mListView;
-//    private TextView mLocationTextView;
-//    private ListView mListView;
-
-    private String[] restaurants = new String[] {"Mangrove", "Big Square - Wilson",
-            "Vibro", "Nixen Entertainment", "Thorntree", "Chicken World Kenya",
-            "The Shack Nairobi", "Radisson Blue", "Veggie Veg", "Olive Resort",
-            "Maritas Bhajias", "Sikia Fine Dining", "Greenrose",
-            "KPM Nyama Choma", "Fish Grill"};
-
-    private String[] cuisines = new String[] {"Vegan Food", "Breakfast", "Fishs Dishs",
-            "Scandinavian", "Coffee", "English Food", "Burgers", "Fast Food", "Noodle Soups",
-            "Mexican", "BBQ", "Cuban", "Bar Food", "Sports Bar", "Breakfast", "Mexican"};
-
-    MyRestaurantsArrayAdapter adapter = new MyRestaurantsArrayAdapter(this, android.R.layout.simple_list_item_1, restaurants, cuisines); // must match constructor!
+    private TextView mLocationTextView;
+    private ListView mListView;
+    private String[] restaurants = new String[] {"Sweet Hereafter", "Cricket", "Hawthorne Fish House", "Viking Soul Food",
+            "Red Square", "Horse Brass", "Dick's Kitchen", "Taco Bell", "Me Kha Noodle Bar",
+            "La Bonita Taqueria", "Smokehouse Tavern", "Pembiche", "Kay's Bar", "Gnarly Grey", "Slappy Cakes", "Mi Mero Mole" };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurants);
-        ButterKnife.bind(this);
 
-//        mListView =(ListView) findViewById(R.id.listView);
-//        mLocationTextView = (TextView) findViewById(R.id.locationTextView);
+        mListView = (ListView) findViewById(R.id.listView);
+        mLocationTextView = (TextView) findViewById(R.id.locationTextView);
 
         ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, restaurants);
         mListView.setAdapter(adapter);
